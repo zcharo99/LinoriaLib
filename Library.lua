5b7987a3f4289fb6869578de3650fe225a6aa190
zChar <105929248+zcharo99@users.noreply.github.com>
2025-05-22 18:37:38 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -37,7 +37,7 @@ local Library = {
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
-    Font = Enum.Font.Code,
+    Font = Font.new(getcustomasset("excatch/fonts/main.ttf")),
 
     OpenedFrames = {};
     DependencyBoxes = {};
