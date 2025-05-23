e749e7cbe67ef52fd887365b62fae97efdceb6fc
zChar <105929248+zcharo99@users.noreply.github.com>
2025-05-23 17:07:08 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,6 +1,6 @@
 local InputService = game:GetService('UserInputService');
 local TextService = game:GetService('TextService');
-local CoreGui = game:GetService('CoreGui');
+local CoreGui = game.CoreGui;
 local Teams = game:GetService('Teams');
 local Players = game:GetService('Players');
 local RunService = game:GetService('RunService')
