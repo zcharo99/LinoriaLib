e443566c8c6582d5ca97de3254b7263e388f45bd
zChar <105929248+zcharo99@users.noreply.github.com>
2025-05-23 17:46:41 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2698,7 +2698,7 @@ do
 
     local WatermarkOuter = Library:Create('Frame', {
         BorderColor3 = Color3.new(0, 0, 0);
-        Position = UDim2.new(0, 100, 0, -25);
+        Position = UDim2.new(0, 50, 0, -25);
         Size = UDim2.new(0, 213, 0, 20);
         ZIndex = 200;
         Visible = false;
