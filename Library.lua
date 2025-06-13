dc3e489c0c7cb49099cb8ba0b9a0fd71aaf3e595
zChar <105929248+zcharo99@users.noreply.github.com>
2025-06-13 14:48:10 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -8,7 +8,7 @@ local GuiService = game:GetService('GuiService');
 local RenderStepped = RunService.RenderStepped;
 local LocalPlayer = Players.LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
-local CoreGui = game.GetService('CoreGui');
+local CoreGui = game:GetService('CoreGui');
 
 -- local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
